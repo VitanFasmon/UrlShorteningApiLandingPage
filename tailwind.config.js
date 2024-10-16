48c1d6b81f1e91ccabf645930f658cb5b009ed76
Vitan Fasmon <vitan.fasmon@gmail.com>
2024-10-16 12:47:29 +0200

    add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // adjust paths based on your project structure
+  ],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "320px", // base minimum for mobile
+      sm: "375px", // design width for mobile
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      desktop: "1440px", // design width for desktop
+    },
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          darkViolet: "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          grayishViolet: "hsl(257, 7%, 63%)",
+          veryDarkBlue: "hsl(255, 11%, 22%)",
+          veryDarkViolet: "hsl(260, 8%, 14%)",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        body: "18px",
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
 };
